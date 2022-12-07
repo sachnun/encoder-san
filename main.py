64ba2727da545b2625baf2087a06d5b845e525f9
sachnun <sachnun12@gmail.com>
2022-12-07 22:45:38 +0700

    build(deps): update 3 files and create 1 file

--- main.py
+++ main.py
@@ -1,48 +1,48 @@
-from flask import Flask, Response
-from flask_restful import Resource, Api, reqparse
+from typing import Union
+from fastapi import FastAPI, Response
+from pydantic import BaseModel
 from subtitle import Subtitle
-from paste import KlgrthPaste
 
-import requests
-
-app = Flask(__name__)
-api = Api(app)
+import aiohttp
 
 
-class SubtitleAPI(Resource):
-    def get(self):
-        return {"hello": "world"}
-
-    def post(self):
-        # get data form request
-        parser = reqparse.RequestParser()
-        parser.add_argument("gd_id_ass", type=str, required=True, location="form")
-        args = parser.parse_args()
-
-        # generate subtitle
-        try:
-            url = "https://gdrive-index.dakunesu.workers.dev/?id=" + args["gd_id_ass"]
-            res = requests.request(
-                "COPY",
-                url,
-            )
-            # return res.text
-            if res.status_code == 200:
-                subs = Subtitle(res.text)
-
-                # return klgrth paste
-                # klgrth = KlgrthPaste()
-                # return {"url": klgrth.paste(subs.generate())}
-
-                # return raw paste
-                return Response(subs.generate(), mimetype="text/plain")
-            else:
-                return {"error": "subtitle not found"}, 404
-        except Exception as e:
-            return {"error": str(e)}, 500
+class Item(BaseModel):
+    id: str
 
 
-api.add_resource(SubtitleAPI, "/")
+app = FastAPI()
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+
+@app.get("/")
+def read_root():
+    return {
+        "title": "Encoder service",
+        "description": "This service is used to encode anything",
+        "version": "0.0.1",
+    }
+
+
+@app.post("/caption")
+async def caption(item: Item):
+    async with aiohttp.ClientSession() as session:
+        async with session.request(
+            "COPY", "https://gdrive-index.dakunesu.workers.dev/?id=" + item.id
+        ) as response:
+
+            if response.status != 200:
+                return Response(status_code=404)
+
+            subs = Subtitle(await response.text())
+            return Response(content=subs.generate(), media_type="text/vtt")
+
+
+# if 500 error, check the log
+@app.exception_handler(Exception)
+async def exception_handler(request, exc):
+    return Response(
+        content={
+            "error": "Internal server error",
+            "message": str(exc),
+        },
+        status_code=500,
+    )
