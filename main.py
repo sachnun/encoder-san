5cf9e6e2b8434d532b8159df7a62c90c0f058d92
sachnun <sachnun12@gmail.com>
2023-01-01 23:40:22 +0700

    update settings.json and main.py

--- main.py
+++ main.py
@@ -46,3 +46,9 @@ async def exception_handler(request, exc):
         },
         status_code=500,
     )
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
